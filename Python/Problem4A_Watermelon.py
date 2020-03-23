fbc5b6871bb058f4116b6726d712b24b8783c224
Muhammed Süha Demirel <msuhademirel@gmail.com>
2020-03-23 21:50:05 +0300

    Problem4A

---
+++ Python/Problem4A_Watermelon.py
@@ -0,0 +1,11 @@
+def isEven(weight):
+    if weight % 2 or weight == 2:
+        print("NO")
+    else:
+        print("YES")
+
+def main():
+    isEven(int(input()))
+
+if __name__ == "__main__":
+    main()
